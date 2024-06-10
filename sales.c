3436ed11ab74c4ebb02341a374e95a850d6aea44
Maya Routh <routhmaya@gmail.com>
2024-06-09 21:10:19 -0500

    read and print 12 lines from input file

--- sales.c
+++ sales.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//accept 20 characters max per line
+#define MAX_CHARS 20
+
 int main() {
     FILE* filePointer;
+    char sales[MAX_CHARS];
 
     filePointer = fopen("input.txt", "r");
+    
+    //write 12 numbers to sales
+    for (int i = 0; i < 12; i++) {
+        fgets(sales, MAX_CHARS, filePointer);
+        printf("%s", sales, "\n"); 
+    }
 
     return 0;
 }
