47f7a5d7cc65a63d5a89ed3c6b0b3a04f9f8063b
Maya Routh <routhmaya@gmail.com>
2024-06-09 21:56:05 -0500

    print sales with months

--- sales.c
+++ sales.c
@@ -6,15 +6,26 @@
 
 int main() {
     FILE* filePointer;
-    char sales[MAX_CHARS];
+    char buf[MAX_CHARS];
+    char sales[12][MAX_CHARS];
+    char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
     filePointer = fopen("input.txt", "r");
     
     //write 12 numbers to sales
-    for (int i = 0; i < 12; i++) {
-        fgets(sales, MAX_CHARS, filePointer);
-        printf("%s", sales, "\n"); 
+    for (int i=0; i<12; i++) {
+        fgets(buf, MAX_CHARS, filePointer);
+        for (int j=0; j<20; j++) {
+            sales[i][j] = buf[j];
+        }
     }
+
+    printf("Monthly sales report:\n");
+    for (int i=0; i<12; i++) {
+        printf("%s:\t%s", months[i], sales[i]);
+    }
+
+    fclose(filePointer);
 
     return 0;
 }
