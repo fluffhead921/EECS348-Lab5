567c00067941c6c628284e84c471702648f50702
Maya Routh <routhmaya@gmail.com>
2024-06-13 15:19:56 -0500

    minimal case added

--- football.c
+++ football.c
@@ -4,13 +4,20 @@
 int main() {
     int score;
     int points[] = {8, 7, 6, 3, 2};
-    int td2Count;
-    int td1Count;
-    int tdCount;
-    int fgCount;
-    int safetyCount;
+    char *name[] = {"TD + 2pt", "TD + 1pt", "TD", "FG", "Safety"};
+    int count[5];
 
     printf("Enter 0 or 1 to stop\nEnter NFL score: ");
-    scanf("%d", score);
-
+    scanf("%d", &score);
+    
+    int tempScore = score;
+    for (int j=0; j<5; j++) {
+        count[j] = 0;
+        while ((tempScore - points[j] > 1) || (tempScore == points[j])) { //no point value of 1 exists
+            tempScore -= points[j];
+            count[j]++;
+        }
+        printf("%d %s, ", count[j], name[j]);
+    }
+    printf("\n");
 }
