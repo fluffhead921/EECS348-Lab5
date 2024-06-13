f5ac87e92d7869cbe04d5656c51eaf66da2f29f5
Maya Routh <routhmaya@gmail.com>
2024-06-13 18:05:52 -0500

    finds counts recursively

--- football.c
+++ football.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int score;
-    int points[] = {8, 7, 6, 3, 2};
-    char *name[] = {"TD + 2pt", "TD + 1pt", "TD", "FG", "Safety"};
-    int count[5];
+int score;
+int points[] = {8, 7, 6, 3, 2};
+char *name[] = {"TD + 2pt", "TD + 1pt", "TD", "FG", "Safety"};
+int count[] = {0, 0, 0, 0, 0};
 
+void generateCounts(int tempScore1, int index) {
+    if (index > 4) {
+        return;
+    }
+    int maxCountI = tempScore1 / points[index];
+    if (tempScore1 % points[index] == 1) {
+        maxCountI--;
+        if (index == 4) {
+            count[3]++;
+        }
+    }
+    for (int j = 0; j <= maxCountI; j++) {
+        int tempScore2 = tempScore1;
+        tempScore2 -= j * points[index];
+        count[index] = j;
+        generateCounts(tempScore2, (index + 1));
+        int total = 0;
+        for (int i = 0; i < 5; i++) {
+            total += count[i] * points[i];
+        }
+        if (total == score) {
+            for (int i = 0; i < 4; i++) {
+                printf("%d %s, ", count[i], name[i]);
+            }
+            printf("%d %s\n", count[4], name[4]);
+        }
+    }
+}
+
+int main() {
     printf("Enter 0 or 1 to stop\nEnter NFL score: ");
     scanf("%d", &score);
     
-    for (int i = 0; i < 5; i++) {
-        int tempScore = score;
-        for (int j = i; j < 5; j++) {
-            count[j] = 0;
-            while ((tempScore - points[j] > 1) || (tempScore == points[j])) { //no point value of 1 exists
-                tempScore -= points[j];
-                count[j]++;
-            }
-            printf("%d %s, ", count[j], name[j]);
-        }
-        printf("\n");
-    }
+    generateCounts(score, 0);
 }
