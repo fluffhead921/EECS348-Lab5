c3622d2e7dfed392fa9322dbcca7d9f278e2e5a1
Maya Routh <routhmaya@gmail.com>
2024-06-13 14:41:11 -0500

    added point values and counts

--- football.c
+++ football.c
@@ -3,7 +3,14 @@
 
 int main() {
     int score;
+    int points[] = {8, 7, 6, 3, 2};
+    int td2Count;
+    int td1Count;
+    int tdCount;
+    int fgCount;
+    int safetyCount;
 
     printf("Enter 0 or 1 to stop\nEnter NFL score: ");
     scanf("%d", score);
+
 }
