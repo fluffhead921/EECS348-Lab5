3229653b3381b3dbed22eda089a8a80169df5296
Maya Routh <routhmaya@gmail.com>
2024-06-13 20:53:27 -0500

    cleanup, no functional changes

--- football.c
+++ football.c
@@ -7,27 +7,33 @@ char *name[] = {"TD + 2pt", "TD + 1pt", "TD", "FG", "Safety"};
 int count[] = {0, 0, 0, 0, 0};
 
 void generateCounts(int tempScore1, int index) {
+    //do not accept indices not in arrays
     if (index > 4) {
         return;
     }
+    //calculate the maximum count of the point value of the current index
     int maxCountI = tempScore1 / points[index];
-    if (tempScore1 % points[index] == 1) {
+    if (tempScore1 % points[index] == 1) { //no point value 1 exists so 3 must be used
         maxCountI--;
         if (index == 4) {
             count[3]++;
         }
     }
     int tempScore2;
-    for (int j = 0; j <= maxCountI; j++) {
+    //find counts for combinations up to maximum
+    for (int i = 0; i <= maxCountI; i++) {
         tempScore2 = tempScore1;
-        tempScore2 -= j * points[index];
-        count[index] = j;
+        tempScore2 -= i * points[index];
+        count[index] = i;
+        //find counts for next index with the remaining score
         generateCounts(tempScore2, (index + 1));
     }
+    //find current total score from counts
     int total = 0;
     for (int i = 0; i < 5; i++) {
         total += count[i] * points[i];
     }
+    //ensures only valid combinations are printed
     if (total == score && index == 4 && tempScore2 == 0) {
         for (int i = 0; i < 4; i++) {
             printf("%d %s, ", count[i], name[i]);
@@ -37,8 +43,10 @@ void generateCounts(int tempScore1, int index) {
 }
 
 int main() {
+    //get score input
     printf("Enter 0 or 1 to stop\nEnter NFL score: ");
     scanf("%d", &score);
     
+    //print combinations
     generateCounts(score, 0);
 }
