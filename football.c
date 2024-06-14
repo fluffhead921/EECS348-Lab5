1115ae23a43b0b2d53ba2a0c9458258d2b95a42d
Maya Routh <routhmaya@gmail.com>
2024-06-13 20:40:02 -0500

    fixed combination repetition bug

--- football.c
+++ football.c
@@ -17,21 +17,22 @@ void generateCounts(int tempScore1, int index) {
             count[3]++;
         }
     }
+    int tempScore2;
     for (int j = 0; j <= maxCountI; j++) {
-        int tempScore2 = tempScore1;
+        tempScore2 = tempScore1;
         tempScore2 -= j * points[index];
         count[index] = j;
         generateCounts(tempScore2, (index + 1));
-        int total = 0;
-        for (int i = 0; i < 5; i++) {
-            total += count[i] * points[i];
+    }
+    int total = 0;
+    for (int i = 0; i < 5; i++) {
+        total += count[i] * points[i];
+    }
+    if (total == score && index == 4 && tempScore2 == 0) {
+        for (int i = 0; i < 4; i++) {
+            printf("%d %s, ", count[i], name[i]);
         }
-        if (total == score) {
-            for (int i = 0; i < 4; i++) {
-                printf("%d %s, ", count[i], name[i]);
-            }
-            printf("%d %s\n", count[4], name[4]);
-        }
+        printf("%d %s\n", count[4], name[4]);
     }
 }
 
